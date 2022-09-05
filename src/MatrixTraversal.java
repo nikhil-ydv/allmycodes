6b9f94a6bf3878d5080ecc477f93a3f222fd131f
nikhil-ydv <ynikhil95@gmail.com>
2022-09-05 19:05:23 +0530

    Matrix Traversal, rowwise, columnwise, diagonal, all diagonal

--- src/MatrixTraversal.java
+++ src/MatrixTraversal.java
@@ -25,7 +25,7 @@ public class MatrixTraversal {
                 System.out.print(arr2D[col][row]+" ");
             }
         }
-        //column top to bottom
+        //column bottom to top
         System.out.println();
         System.out.println("Print  bottom to top column wise");
 
@@ -54,6 +54,35 @@ public class MatrixTraversal {
             k++;
             l--;
         }
+        //all diagonals
+//        1 2 3      first part where diagonal starts from first columns , other part diagonal start from last row
+//        4 5 6
+//        7 8 9
+        System.out.println();
+        System.out.println("All Diagonals");
+        for(int d = 0; d < arr2D.length; d++) {
+             i = d;
+             j = 0;
+             while(i >= 0) {
+                 System.out.print(arr2D[i][j] + " ");
+                 i--;
+                 j++;
+             }
+            System.out.println();
+
+        }
+        //second part
+        for(int d = 1; d < arr2D[0].length; d++) {
+            i = arr2D[0].length - 1;
+            j = d;
+            while(j <= arr2D[0].length - 1) {
+                System.out.print(arr2D[i][j] + " ");
+                i--;
+                j++;
+            }
+            System.out.println();
+        }
+
 
 
 
